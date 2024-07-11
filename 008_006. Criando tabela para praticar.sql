d03c1197f9f30b19b7484df46c2c876533399c61
Victor Alejandro <106128743+alejandroxb@users.noreply.github.com>
2024-07-11 15:00:20 -0300

    008_006. Criando tabela para praticar.sql

---
+++ 008_006. Criando tabela para praticar.sql
@@ -0,0 +1,12 @@
+CREATE TABLE marcas(
+	id_marca int,
+	marca varchar(30))
+
+INSERT INTO marcas(id_marca, marca)
+VALUES
+	(1, 'Apple'),
+	(2, 'Samsung'),
+	(3, 'Motorola')
+
+SELECT FROM * marcas
+SELECT FROM * subcategorias
