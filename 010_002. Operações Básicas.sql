dac0fe5c9f6ad5d7d428300e366fb9c0c5c61e79
Victor Alejandro <106128743+alejandroxb@users.noreply.github.com>
2024-08-01 21:54:32 -0300

    Create 010_002. Operações Básicas.sql

---
+++ 010_002. Operações Básicas.sql
@@ -0,0 +1,23 @@
+-- [SQL Server] Variáveis
+-- Operações básicas
+
+SELECT 10 AS 'Número'
+
+SELECT 'Marcus' AS 'Nome'
+
+SELECT '21/06/2021' AS 'Data'
+
+-- Operações com números
+
+SELECT 10+20
+SELECT 20-5
+SELECT 31*40
+SELECT 431.0/23
+
+-- Operações com textos
+
+SELECT 'SQL' + ' ' + 'Impressionador'
+
+-- Operações com datas
+
+SELECT '21/03/2021' + 1
