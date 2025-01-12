968d1d895f1af35c5e7dfa7e942b56b1c317a5fb
ᴠɪᴄᴛᴏʀ ᴀʟᴇᴊᴀɴᴅʀᴏ <106128743+vicchenzo@users.noreply.github.com>
2025-01-11 23:33:16 -0300

    Update 001_001. ORDER BY - Parte_I.sql

--- 001_001. ORDER BY - Parte_I.sql
+++ 001_001. ORDER BY - Parte_I.sql
@@ -2,17 +2,57 @@ SELECT * FROM DimProduct
 
 SELECT TOP (10)
 	UnitPrice
-FROM 
-	DimProduct
-ORDER BY
-	UnitPrice DESC
+FROM DimProduct
+ORDER BY UnitPrice DESC
 
 -- QUANTOS PRODUTOS TEM UM PREÇO UNITARIO MAIOR QUE $1000
 
 SELECT
 	ProductName AS 'Produto',
 	unitprice AS 'Preço'
-FROM
-	DimProduct
-WHERE
-	UnitPrice >= 1000
+FROM DimProduct
+WHERE UnitPrice >= 1000
+
+-- PARTE II
+SELECT * FROM DimStore
+
+-- QUANTIDADE DE FUNCIONÁRIOS
+SELECT SUM(EmployeeCount)
+FROM DimStore
+
+-- SUBDIVIDIR QUANTOS FUNCIONÁRIOS POR TIPO DE LOJA
+SELECT
+	StoreType AS 'Loja',
+	SUM(EmployeeCount) AS 'Funcionários'
+FROM DimStore
+GROUP BY StoreType
+
+-- CALCULAR QUAL A MÉDIA DE CUSTO DOS PRODUTOS
+SELECT * FROM DimProduct
+
+SELECT
+	BrandName AS 'Marca',
+	AVG(UnitCost) AS 'Média de Custo'
+FROM DimProduct
+GROUP BY BrandName
+
+-- QUAL MAXIMO DE VALOR DE PREÇO UNITÁRIO[UnitCost] SEGUNDO A COLUNA DE ClassName
+
+SELECT
+	ClassName, -- AGRUPAMENTO FEITO POR ClassName
+	MAX(UnitPrice) AS 'Máximo Preço' -- CÁLCULO QUE QUEREMOS FAZER É O MÁXIMO UnitPrice
+FROM DimProduct
+GROUP BY ClassName
+
+-- ORDER BY + WHERE
+SELECT * FROM DimProduct
+
+-- AS LINHAS ONDE A MARCA É FABRIKAM
+SELECT 	*
+FROM DimProduct
+WHERE BrandName = 'Fabrikam'
+
+-- AS LINHAS ONDE A COR DO PRODUTO SEJAM BLACK
+SELECT *
+FROM DimProduct
+WHERE ColorName = 'Black'
