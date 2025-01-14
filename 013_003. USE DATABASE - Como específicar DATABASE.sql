0c86b329c93b235222f92800f79511d97d30021a
ᴠ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-13 22:37:42 -0300

    Create 013_003. USE DATABASE - Como específicar DATABASE.sql

---
+++ 013_003. USE DATABASE - Como específicar DATABASE.sql
@@ -0,0 +1,5 @@
+-- USE DATABASE
+-- Basicamente quando usamos o USE mudamos o banco de dados que está no Menu acima, portanto, quando temos dois bancos de dados distintos e dermos um SELECT os dois bancos de dados aparecerão, porém, o último que constará no nosso MENU
+
+USE ContosoRetailDW
+SELECT * FROM DimProduct
