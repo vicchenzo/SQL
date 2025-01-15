7849453754f08a7ba54e85778b7f6dc618670067
ᴠ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-14 22:49:56 -0300

    Create 013_005. DROP VIEW.sql

---
+++ 013_005. DROP VIEW.sql
@@ -0,0 +1,9 @@
+-- DROP VIEW
+-- Exclua as VIEWS vwClientes e vwProdutos
+
+-- Podemos fazer isso manualmente indo no banco de dados clicando com botão direito em cima do banco de dados 'Excluir'
+
+-- SEGUNDA MANEIRA: Comando DROP VIEW
+-- Quando executamos o código a VIEW será excluída
+DROP VIEW vwClientes
+DROP VIEW vwProdutos
