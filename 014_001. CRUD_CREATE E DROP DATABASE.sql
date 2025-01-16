33a915c2cc46c4c7f6ba6a046a6d872c14ec897b
ᴠ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-15 22:41:17 -0300

    Create 014_001. CRUD_CREATE E DROP DATABASE.sql

---
+++ 014_001. CRUD_CREATE E DROP DATABASE.sql
@@ -0,0 +1,10 @@
+-- Criando e deletando um Banco de dados
+
+CREATE DATABASE Teste
+
+
+-- Deletando Base de dados
+DROP DATABASE Teste
+
+-- VAMOS TRABALHAR COM O BANCO DE DADOS ABAIXO
+CREATE DATABASE BDImpressionador
