26b0ae4f3a1a99c4be62c4d6878cb24d029ac04e
ᴠ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-15 22:43:24 -0300

    Update 014_001. CRUD_CREATE E DROP DATABASE.sql

--- 014_001. CRUD_CREATE E DROP DATABASE.sql
+++ 014_001. CRUD_CREATE E DROP DATABASE.sql
@@ -8,3 +8,8 @@ DROP DATABASE Teste
 
 -- VAMOS TRABALHAR COM O BANCO DE DADOS ABAIXO
 CREATE DATABASE BDImpressionador
+
+--  Cuidados antes de criar uma Tabela em um banco de dados:
+-- Quando criamos uma tabela podemos escolher qual base de dados podemos adicionar essa tabela, para não ter erros selecionamos a Base de dados utilizando 'USE'
+
+USE BDImpressionador
