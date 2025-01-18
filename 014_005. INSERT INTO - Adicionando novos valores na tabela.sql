b1b81a1466e315d204879d94211438ec7229617a
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-17 22:46:03 -0300

    Create 014_005. INSERT INTO - Adicionando novos valores na tabela.sql

---
+++ 014_005. INSERT INTO - Adicionando novos valores na tabela.sql
@@ -0,0 +1,8 @@
+-- 005. INSERT INTO. Adicionando novos valores na tabela
+
+INSERT INTO Produtos(id_produto, nome_produto, data_validade, preco_produto)
+VALUES
+	(1, 'Arroz', '31/12/2021', 22.50),
+	(2, 'Feijão', '20/11/2022', 8.99)
+
+SELECT * FROM Produtos
