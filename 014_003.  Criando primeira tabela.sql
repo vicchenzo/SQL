ec9b222725d17bd81b57e82001ce0f6080ac153e
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-17 22:42:52 -0300

    Create 014_003.  Criando primeira tabela.sql

---
+++ 014_003.  Criando primeira tabela.sql
@@ -0,0 +1,21 @@
+-- 003. CREATE TABLE - Criando a primeira tabela
+
+-- Crie uma tabela chamada PRODUTOS
+-- Esa tabela deve conter 4 colunas: id_produto, nome_produto, data_validade, preco_produto
+-- Certifique-se de que os tipos das colunas está correto
+
+-- PADRÃO DE CRIAÇÃO DE TABELA
+-- Coluna1 TIPO1,
+--		Coluna2 TIPO2,
+--		Coluna3 TIPO3
+
+USE BDImpressionador
+
+ CREATE TABLE Produtos(
+	id_produto INT,
+	nome_produto VARCHAR(200),
+	data_validade DATETIME,
+	preco_produto FLOAT
+)
+
+SELECT * FROM Produtos
