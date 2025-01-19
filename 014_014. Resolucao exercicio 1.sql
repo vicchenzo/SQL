0df1140e01e18ae22918b6b75dfdfac79c50bfa1
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-18 22:35:08 -0300

    Create 014_014. Resolucao exercicio 1.sql

---
+++ 014_014. Resolucao exercicio 1.sql
@@ -0,0 +1,11 @@
+-- Exercício 1
+-- a) Crie um banco de dados chamado BD_Teste.
+CREATE DATABASE BD_Teste
+
+-- b) Exclua o banco de dados criado no item anterior.
+
+DROP DATABASE BD_Teste
+
+-- c) Crie um banco de dados chamado Exercicios.
+CREATE DATABASE Exercicios
+
