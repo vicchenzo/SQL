7cd45b2f89fe012494fae55387180f1d322c83d6
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-27 22:49:23 -0300

    Delete 002. FUNÇÕES DE AGREÇÃO.sql

--- 002. FUNÇÕES DE AGREÇÃO.sql
+++
@@ -1,8 +0,0 @@
-SELECT	TOP(100) * -- Mostra o TOP 100
-FROM	FactSales
-
-  -- SOMA DE TOTAL VENDIDO / TOTAL DEVOLVIDO
-SELECT
-	SUM(SalesQuantity) as 'Total Vendido',
-	SUM(ReturnQuantity) as 'Total Devolvido'
-FROM FactSales
