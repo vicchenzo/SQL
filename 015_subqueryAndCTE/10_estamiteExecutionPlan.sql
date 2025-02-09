1452ea355b368b2e6ff34ce6dc4bd4dbaa0b1380
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-08 23:53:03 -0300

    Update and rename [Subqueries e CTE's] Aula 11 de 28 - Plano de Execução Estimado.sql to 10_estamiteExecutionPlan.sql

--- 015_subqueryAndCTE/10_estamiteExecutionPlan.sql
+++ 015_subqueryAndCTE/10_estamiteExecutionPlan.sql
@@ -1,4 +1,4 @@
--- [SQL Server] [Subqueries e CTE's] Aula 11 de 28: Plano de Execução Estimado
+-- [SQL Server] [Subqueries e CTE's] Aula 11 de 28: Plano de ExecuÃ§Ã£o Estimado
 
 -- Exemplo: Retornar a quantidade total de produtos da marca Contoso.
 
