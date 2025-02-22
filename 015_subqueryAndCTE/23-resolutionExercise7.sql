f27dde44a4f6f8cf4a0fee60538ef020538a9fc8
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-21 23:13:01 -0300

    Create 23-resolutionExercise7.sql

---
+++ 015_subqueryAndCTE/23-resolutionExercise7.sql
@@ -0,0 +1,16 @@
+/*7. Você deverá criar uma consulta para o setor de vendas que mostre as seguintes colunas da 
+tabela DimProduct:
+ProductKey,
+ProductName,
+BrandName,
+UnitPrice
+Média de UnitPrice.*/
+
+SELECT  
+	ProductKey,
+	ProductName,
+	BrandName,
+	UnitPrice,
+	ROUND((SELECT AVG(UnitPrice) FROM DimProduct), 2) AS 'AVG UnitPrice'
+FROM 
+	DimProduct
