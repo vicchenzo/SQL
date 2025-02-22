af520445243e5b8e9908df9f86e8d5074cfa598c
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-21 23:17:13 -0300

    Update 01.sql

--- 01_Introduction/01.sql
+++ 01_Introduction/01.sql
@@ -1,1 +1,20 @@
+-- PRIMEIRO COMANDO PARA SELECIONAR TODAS AS COLUNAS DE UMA TABELA
+SELECT * FROM DimCustomer
+
+SELECT * FROM DimStore
+
+SELECT 
+	StoreKey, 
+	StoreName, 
+	StorePhone 
+FROM 
+	DimStore
+
+SELECT * FROM DimProduct
+
+SELECT 
+	ProductName, 
+	BrandName 
+FROM 
+	DimProduct
 
